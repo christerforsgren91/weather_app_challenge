d89cc0905091d9a842e9945748b74c57bba4ec7b
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-14 13:22:30 +0200

    first test for checking display of  temperature and location

---
+++ cypress/integration/DisplayWeatherAtLocation.feature.js
@@ -0,0 +1,22 @@
+describe('weather info is displayed at user location', () => {
+  it('is expected to be displayed on initial render', () => {
+    cy.visit("/")
+      onbeforeunload(window) {
+        const stubLocation = {
+          coords: {
+            lattitude: 55.7842,
+            longitude: 12.4518
+          }
+        }
+        cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
+          callback => {
+            return callback(stubLocation)
+          }
+        )
+      }
+    cy.get("[data-cy=weather-display]").within(() => {
+      cy.get("[data-cy=temp]").should("contain", "17°C")
+      cy.get("[data-cy=location]").should("contain", "Virum")
+    })
+  });
+})
