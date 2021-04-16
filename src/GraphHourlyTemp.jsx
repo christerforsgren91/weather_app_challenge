be39eca70628b342c762065d951192cb94a2afb2
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-16 17:02:59 +0200

    displays graph, but date lables are off

--- src/GraphHourlyTemp.jsx
+++ src/GraphHourlyTemp.jsx
@@ -3,15 +3,31 @@ import Plot from 'react-plotly.js';
 
 const GraphHourlyTemp = (props) => { 
   let hourTemp = []
+  let hourTime = []
+  let newHours = null
+  let date = null
   if(props.data) {
-    props.data.forEach(hour => {
-      hourTemp.push(hour.temp)
+    props.data.forEach(hour => {      
+      hourTime.push(hour.dt)
+      hourTemp.push(hour.temp)      
     })
   }
+  let data = [
+    {
+    x: hourTime,
+    y: hourTemp,
+    x: 'date',
+    type: 'line',
+    //mode: 'lines+markers',
+    //marker: {color: 'red'},
+  },
+]
+  
+  
     return (
       <Plot
-        data={props.data}
-        layout={ {width: 640, height: 480, margin: {l: 0, r: 0, t: 0, b: 20} } }
+        data={data}
+        layout={ {width: 640, height: 480, margin: {l: 20, r: 20, t: 20, b: 20} } }
         config={ {staticPlot: true} }        
       />
     );  
@@ -30,6 +46,7 @@ export default GraphHourlyTemp
   },          
 ]
 */
+//new Date(hour.dt*1000).getHours()
 
 
 
